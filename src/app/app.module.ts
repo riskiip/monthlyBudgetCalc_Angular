45a9f06019f579fcdc7b6c67a8441ddaa6ec4b25
riskiip <muhamadrizki19@student.undip.ac.id>
2020-08-11 10:15:30 +0700

    Menambah component budgetItemList

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponentComponent } from './main-component/main-component.component';
 import { AddItemFormComponent } from './add-item-form/add-item-form.component';
+import { BudgetItemListComponent } from './budget-item-list/budget-item-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponentComponent,
-    AddItemFormComponent
+    AddItemFormComponent,
+    BudgetItemListComponent
   ],
   imports: [
     BrowserModule,
